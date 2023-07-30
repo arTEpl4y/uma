2e0d3971b8c2a483256eff4cec72e6553da201e3
arTEpl4y <arteg4me@gmail.com>
2023-07-30 19:22:02 +0200

    background click doesnt work, switching to pyautogui, script will probably be used on VM

--- tob.py
+++ tob.py
@@ -1,15 +1,15 @@
-from pyautogui import *
-import pyautogui
 import time
 import keyboard
 import numpy as np
 import random
+import autoit
 from ctypes import windll
 from PIL import Image
 import cv2 as cv
 import win32api
 import win32con
 import win32gui
+import win32com
 import win32ui
 
 time.sleep(2)
@@ -64,15 +64,14 @@ def takess(hwnd):  # show cloned game window using cv2
 
     return cv.cvtColor(img, cv.COLOR_RGBA2RGB)
 
-def click(x, y): # click?
-
+def click(x, y):
     hWnd = win32gui.FindWindow(None, "umamusume")
-    lParam = win32api.MAKELONG(x+16, y+39)
 
-    hWnd1= win32gui.FindWindowEx(hWnd, None, None, None)
-    win32gui.SendMessage(hWnd1, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
-    time.sleep(np.random.uniform(0.1,0.3))
-    win32gui.SendMessage(hWnd1, win32con.WM_LBUTTONUP, None, lParam)
+    lParam = win32api.MAKELONG(x, y-39)
+
+    win32api.PostMessage(hWnd, win32con.BM_CLICK, win32con.MK_LBUTTON, lParam)
+    time.sleep(0.1)
+    win32api.PostMessage(hWnd, win32con.WM_LBUTTONUP, None, lParam)
 
 # script start
 win_uma = win32gui.FindWindow(None, "umamusume")
@@ -81,11 +80,10 @@ width = right - left - 16
 height = bottom - top - 39
 
 savess(win_uma,"ss.png")
-click(410, 960)
+
 while (True):
 
     ss = takess(win_uma)
-
     cv.imshow('cv2 - umamusume', ss)
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
