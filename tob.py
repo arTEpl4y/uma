c974d8255ec058fb04dcad3262151f78c444c0dd
arTEpl4y <arteg4me@gmail.com>
2023-08-06 14:34:06 +0200

    fuck me, man...

--- tob.py
+++ tob.py
@@ -1,7 +1,6 @@
 from ctypes import windll
-from platform import win32_is_iot
-from tkinter import Image
 from pyautogui import *
+import keyboard
 import pyautogui
 import time
 import numpy as np
@@ -12,8 +11,6 @@ import win32gui
 import win32ui
 
 time.sleep(2)
-# best debugging line known to mankind
-print("helo world")
 
 def click(x, y):    # click
     win32api.SetCursorPos((x, y))
@@ -21,59 +18,18 @@ def click(x, y):    # click
     time.sleep(np.random.uniform(0.1, 0.35))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-
-win_uma = win32gui.FindWindow(None, "umamusume")
-
-
-def takescreen(hwnd, width, height, filename):
-    # hwnd is window handle
-    # width, height are in pixels
-    # filename is name of screenshot file
-
-    hwndDC = win32gui.GetWindowDC(hwnd)
-    mfcDC = win32_is_iot.CreateDCFromHandle(hwndDC)
-    saveDC = mfcDC.CreateCompatibleDC()
-
-    saveBitMap = win32ui.CreateBitmap()
-    saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
-    saveDC.SelectObject(saveBitMap)
-    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
-    bmpinfo = saveBitMap.GetInfo()
-    bmpstr = saveBitMap.GetBitmapBits(True)
-    im = Image.frombuffer(
-        'RGB',
-        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-        bmpstr, 'raw', 'BGRX', 0, 1)
-
-    win32gui.DeleteObject(saveBitMap.GetHandle())
-    saveDC.DeleteDC()
-    mfcDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, hwndDC)
-
-    if result == 1:
-        # PrintWindow Succeeded
-        im.save(filename)
-
-
-# sample usage
-hwnd = win32gui.FindWindow(None, 'Chrome')
-takescreen(hwnd, 1024, 768, 'screenshot.png')
-
-# best debugging line known to mankind
-print("helo world")
-
-# while keyboard.is_pressed('q') == False:
-
-# if pyautogui.locateOnScreen('comparision_pngs/button.png', grayscale=True, confidence=0.7) != None:
-#    click(np.random.randint(852,1053),np.random.randint(854,896))
-#    time.sleep(np.random.uniform(2,7))
-
-# script start
-
-# training options
-
-
-def go_into_train_options():
-    if pyautogui.locateOnScreen('comparision_pngs\button_oguri_train.png', grayscale=True, confidence=0.7) != None:
-        click(np.random.randint(859, 1043), np.random.randint(773, 834))
+def train_options():    # training options
+    if pyautogui.locateOnScreen('comparision_pngs/button_train_oguri.png', grayscale=True, confidence=0.7) != None:
+        click(np.random.randint(870, 1032), np.random.randint(780, 834))
         time.sleep(np.random.uniform(3, 8))
+
+def click_train_wisdom():   #commence wisdom training
+    click(np.random.randint(1119, 1181), np.random.randint(831, 892))
+    time.sleep(np.random.uniform(0.8, 1.5))
+    click(np.random.randint(1119, 1181), np.random.randint(821, 882))
+    
+print("witaj swiecie")  # best debugging line known to mankind
+
+#script start
+train_options()
+click_train_wisdom()
